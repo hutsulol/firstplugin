8a0950ee241881dbe301601a7f6647b5b9b92dce
hutsulol <130280679+hutsulol@users.noreply.github.com>
2026-03-10 12:57:14 +0200

    Creating main plugin class for FirstPlugin

---
+++ src/main/java/com/hutsulol/firstplugin/FirstPlugin.java
@@ -0,0 +1,15 @@
+package com.hutsulol.firstplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FirstPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
